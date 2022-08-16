90787402f0a6580f104e4fac65eba7cb780bf3da
zielinskin <zielinskin@outlook.com>
2022-08-16 17:42:51 -0500

    demonstrate issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.3"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.springframework.boot") version "3.0.0-M3"
 	war
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
-	id("org.jetbrains.kotlin.plugin.jpa") version "1.6.21"
+	kotlin("jvm") version "1.7.10"
+	kotlin("plugin.spring") version "1.7.10"
+	id("org.jetbrains.kotlin.plugin.jpa") version "1.7.10"
+	id("org.ajoberstar.grgit") version "5.0.0"
 }
-
 group = "zielinskin"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+apply(plugin = "io.spring.dependency-management")
 
 repositories {
 	mavenCentral()
@@ -25,6 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("javax.persistence:javax.persistence-api:2.2")
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 
@@ -37,10 +39,22 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+		jvmTarget = "17"
 	}
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+afterEvaluate {
+	val reg =  Regex("^([^0-9]*)?([0-9]*)?(\\.([0-9]*))?(\\.([0-9]*))?((?i)(?!ITC).)*(?i)(ITC-[0-9]*)*.*$")
+	var ver : String = "0.0.0"
+	if(!grgit.tag.list().isEmpty()) {
+		var match = reg.find(grgit.tag.list().last().name);
+		ver = (match?.groups?.get(2)?.value ?: "0") + "." + (match?.groups?.get(4)?.value
+			?: "0") + "." + (match?.groups?.get(6)?.value ?: "0")
+	}
+	logger.log(org.gradle.api.logging.LogLevel.ERROR, "version is: $ver")
+	version = ver
 }
--- src/main/kotlin/zielinskin/kotlinsample/ServletInitializer.kt
+++ src/main/kotlin/zielinskin/kotlinsample/ServletInitializer.kt
@@ -2,10 +2,12 @@ package zielinskin.kotlinsample
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EntityScan("zielinskin.kotlinsample.*")
+@EnableJpaRepositories("zielinskin.kotlinsample.*")
 class ServletInitializer
 
 fun main(args: Array<String>) {
